96f296f9b4fe0e84e60bc417bcb37140dcb575fe
Allison <amh03160@gmail.com>
2022-02-09 14:49:18 -0600

    Completed assignment :)

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solutions in this file
+/*
+for ([initialization]; [condition];[iteration]) {
+    [loop body]
+}
+*/
+/* for (let age = 30; age <40; age ++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+    };
+return gifts;
+};
+
+wrapGifts(gifts);
+*/
+
+//for loop
+function writeCards(names,event){
+    const thanksArray=[];
+    for(let i =0; i< names.length;i++){
+        let message =`Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        thanksArray[i]=message;
+        debugger;
+    };
+    return thanksArray; 
+};
+writeCards(["Charlie", "Samip", "Ali]"],"birthday"); 
+
+//while loop
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts){
+    let i =0; 
+    while(i<gifts.length){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`); 
+        i++;
+    }
+    return gifts; 
+};
+
+wrapGifts(gifts);
+*/
+function countDown(n){
+    while(n>-1) {
+        console.log(n)
+        n--
+    }
+};
